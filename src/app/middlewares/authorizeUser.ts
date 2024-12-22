4ad70117f0457fee772d6664aa510eb457c333d7
shopna akter <shopna88akter@gmail.com>
2024-12-22 17:38:12 +0600

    authentication completed

--- src/app/middlewares/authorizeUser.ts
+++ src/app/middlewares/authorizeUser.ts
@@ -6,10 +6,7 @@ import { User } from '../modules/user/user.model';
 import { verifyToken } from '../utilities/authUtilities';
 import { STATUS_CODES } from '../constants';
 
-/**
- * Middleware to check if the user is authorized to access the route.
- * @param requiredRoles User role/roles (comma separated) required to access the route.
- */
+
 const authorizeUser = (...requiredRoles: TUserRole[]) => {
 	return catchAsync(async (req, _res, next) => {
 		const token = req.headers.authorization?.split(' ')[1];
